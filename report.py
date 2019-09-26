4533afbe93e872209c460fe7b3787824428f80c9
jimbomoso <jimbomoso@gmail.com>
2019-09-26 14:21:21 -0400

    Adds report questions to answer

---
+++ report.py
@@ -0,0 +1,5 @@
+# 1. What are the most popular three articles of al time?
+
+# 2. Who are the most popular article authors of all time?
+
+# 3. On which days did more the 1% of requests lead to errors?
