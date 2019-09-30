c0048197a2a6a4e7614e05f0c4350a36e837e7c4
jimbomoso <jimbomoso@gmail.com>
2019-09-30 14:04:02 -0400

    Adds query for most pop authors

--- newsdb.py
+++ newsdb.py
@@ -37,3 +37,50 @@ order by
   views desc
 fetch first 3 rows only;
 '''
+
+most_popular_article_authors_of_all_time = '''
+select
+  name,
+  sum(views) as views
+from
+  (
+    select
+      *
+    from
+      (
+        select
+          path,
+          count(*) AS Views
+        from
+          log
+        where
+          status like '2%' AND
+          path in
+            (
+              select
+                concat('/article/', slug)
+              from
+                articles
+            )
+        group by
+          path
+      ) a,
+      (
+        select
+          ar.title,
+          au.name,
+          CONCAT('/article/', ar.slug) as path
+        from
+            articles ar
+          join
+            authors au
+          on (ar.author = au.id)
+      ) b
+    where
+      a.path = b.path
+  ) as c
+group by
+  name
+order by
+  views desc;
+'''
