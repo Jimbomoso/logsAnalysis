8a2a193b460f60f781706264acc38703fc7be7b7
jimbomoso <jimbomoso@gmail.com>
2019-09-30 13:46:39 -0400

    adds first query

--- newsdb.py
+++ newsdb.py
@@ -1,2 +1,39 @@
 import psycopg2
 
+most_popular_three_articles_of_all_time = '''
+select
+  title,
+  views
+from
+  (
+    select
+      path,
+      count(*) AS Views
+    from
+      log
+    where
+      status like '2%' AND
+      path in (
+          select
+            concat('/article/', slug)
+          from articles)
+    group by
+      path
+  ) a,
+  (
+    select
+      ar.title,
+      au.name,
+      CONCAT('/article/', ar.slug) as path
+    from
+        articles ar
+      join
+        authors au
+      on (ar.author = au.id)
+  ) b
+where
+  a.path = b.path
+order by
+  views desc
+fetch first 3 rows only;
+'''
