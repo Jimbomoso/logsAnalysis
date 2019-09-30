4c8d3583aadf6f24bf993bd3f301d79c0313c01b
jimbomoso <jimbomoso@gmail.com>
2019-09-30 15:16:47 -0400

    adds printing of questions and query results

--- report.py
+++ report.py
@@ -1,5 +1,20 @@
-# 1. What are the most popular three articles of al time?
+import newsdb
 
-# 2. Who are the most popular article authors of all time?
+LINE = ''' * '%s' - %s %s'''
 
-# 3. On which days did more the 1% of requests lead to errors?
+if __name__ == '__main__':
+    print("1. What are the most popular three articles of all time?")
+    posts = "".join(LINE % (title, views, "views") for title, views in
+                    newsdb.get_most_popular_three_articles_of_all_time())
+    print(posts)
+
+    print("2. Who are the most popular article authors of all time? ")
+    posts = "".join(LINE % (name, views, "views") for name, views in
+                    newsdb.get_most_popular_article_authors_of_all_time())
+    print(posts)
+
+    print("3. On which days did more than 1% of requests lead to errors? ")
+    posts = "".join(LINE % (date, percentage, "errors") for
+                    date, percentage in newsdb.
+                    get_days_where_1percent_plus_of_requests_lead_to_errors())
+    print(posts)
